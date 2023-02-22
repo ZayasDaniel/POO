dcb817b80184ca877e5ddc2e54e5ad3f0c3f02fe
ZayasDaniel <danielzayas17@aragon.unam.mx>
2023-02-21 20:13:29 -0600

    Subiendo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class 	HolaMundo{
+  public static void main(String args[])
+    {
+      Scanner teclado = new Scanner(System.in);
+      String nombre = "Zayas Rios Moises Daniel";
+      System.out.println("Hola Mundo!!!!");
+      System.out.println("Hola " + nombre );
+      
+
+
+
+    }
+}
