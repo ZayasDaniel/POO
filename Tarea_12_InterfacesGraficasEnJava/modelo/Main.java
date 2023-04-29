2597232adc6a1711a58e3948a6b374f5ea966683
ZayasDaniel <danielzayas17@aragon.unam.mx>
2023-04-29 16:59:37 -0600

    Tarea 12

---
+++ Tarea_12_InterfacesGraficasEnJava/modelo/Main.java
@@ -0,0 +1,10 @@
+package ico.modelo;
+
+import ico.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal("Hola Mundo AWT");
+    }
+}
