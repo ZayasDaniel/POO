d064908393b7c8eb0c1874b49ac9e4cef45e032e
ZayasDaniel <danielzayas17@aragon.unam.mx>
2023-05-04 15:58:39 -0600

    Tarea 13

---
+++ Tarea_13_IntroduccionJavaSwing/MainVentanaSwing.java
@@ -0,0 +1,7 @@
+package SwingLayout;
+
+public class MainVentanaSwing {
+    public static void main(String[] args) {
+        VentanaSwing ventana = new VentanaSwing("Centigrados a Farenheid");
+    }
+}
